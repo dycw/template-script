3ea95f64952721f7329c40b84d18cb55886d58f7
Derek Wan <d.wan@icloud.com>
2025-12-24 11:11:53 +0900

    Update `script.py.lock` (#14)

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,4 +7,4 @@ from utilities.pathlib import get_repo_root
 
 class TestScript:
     def test_main(self) -> None:
-        _ = check_call(["./script.py", "--dry-run"], cwd=get_repo_root())
+        _ = check_call(["./script.py"], cwd=get_repo_root())
